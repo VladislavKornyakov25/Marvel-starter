b33ffb4f8f7d4b3b1cf1a8646b4d3ee19c4b91db
Vladislav Kornyakov <time2begin666@gmail.com>
2024-04-07 12:57:28 +0300

    Start

--- src/components/pages/SingleComicPage.js
+++ src/components/pages/SingleComicPage.js
@@ -1,6 +1,6 @@
 import { useParams, Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
-
+import {Helmet} from "react-helmet"
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import AppBanner from '../appBanner/AppBanner';
@@ -32,7 +32,7 @@ const SingleComicPage = () => {
     const content = !(loading || error || !comic) ? <View comic={comic}/> : null;
 
     return (
-        <>
+        <>            
             <AppBanner/>
             {errorMessage}
             {spinner}
@@ -45,6 +45,13 @@ const View = ({comic}) => {
     const {title, description, pageCount,thumbnail, language, price} = comic;
     return (
         <div className="single-comic">
+            <Helmet>
+                <meta
+                    name="description"
+                    content={`${title} comics book`}
+                />
+                <title>{title}</title>
+            </Helmet>
             <img src={thumbnail} alt={title} className="single-comic__img"/>
             <div className="single-comic__info">
                 <h2 className="single-comic__name">{title}</h2>
